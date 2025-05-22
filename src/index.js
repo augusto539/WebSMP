a3075fc7a5dd7caf54e8089550191829b9b786b0
augusto <augustoantonelli88@gmail.com>
2025-05-22 04:26:29 -0300

    cambio un import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 // General style 
-import './Index.css';
+import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
